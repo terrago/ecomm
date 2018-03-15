5792dd8a94ed54af3ef2272fe9a0c5aeaa7cbcfe
Carsten Kleinfeld-Hardt <carsten.kleinfeld-hardt@eucon.com>
2018-03-15 21:59:43 +0100

    first initial commit

---
+++ src/main/java/de/terrago/ecomm/EcommApplication.java
@@ -0,0 +1,12 @@
+package de.terrago.ecomm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommApplication.class, args);
+	}
+}
